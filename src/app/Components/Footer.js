e9b3d479b5d8e2b566123159b797c26d3d86fe6a
0Gimenes <gabrielgimenes9353@gmail.com>
2024-02-29 10:40:59 -0400

    adicionei a imagem do moodle no footer e o link

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -1,7 +1,13 @@
 import styles from "./footer.module.css";
+import Imagem from "next/image";
 export default function Footer(){
     return(
         <footer className={styles.rodape}>
+            <a href="https://ead.ifms.edu.br/">
+            <Imagem width={100}
+            height={100}
+            src={"https://www.logo.wine/a/logo/Moodle/Moodle-Logo.wine.svg"}/>
+            </a>
         <h1>Footer legal</h1>
         </footer>
     );
